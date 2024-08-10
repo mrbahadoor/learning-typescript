ab1a7fa5631470fa9b6190b84d3d7cfc7c3c2e88
Rushdi Bahadoor <my_name@example.com>
2024-08-10 21:09:36 +0400

    Working with core data types

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+function  add(n1:number, n2:number, showResult: boolean, phrase: string) {
+    const result = n1 + n2;
+    if(showResult) {
+        console.log(phrase + result); //Everything is converted to string
+    } else {    
+        return result; //return type is number
+    }
+}
+
+const number1 = 5;
+const number2 = 2.5;
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+add(number1, number2, printResult, resultPhrase);
